e246ece80ededf40bc64caa145f0d7dc1961de7a
tsong0ku <sonbappe89@gmail.com>
2024-05-05 10:57:46 +0700

    rework

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+// import router from '~/router';
+import { lazy } from 'react';
+
+const Home = lazy(() => import("~/pages/Home"));
+
+
+const router = createBrowserRouter([{
+  path: '/',
+  element: <Home />,
+  children: [
+    { path: '/', element: <Home /> },
+
+  ]
+}])
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <RouterProvider router={router} />
+
   </React.StrictMode>,
 )
