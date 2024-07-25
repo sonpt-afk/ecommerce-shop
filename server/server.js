79493c7b5006bd4b1f47b25899a0486497aa4452
Son Pham <78460399+sonpt-afk@users.noreply.github.com>
2024-07-25 09:25:15 +0700

    ssd

--- server/server.js
+++ server/server.js
@@ -1,47 +1,44 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
 // import { corsOptions } from '~/config/cors'
-require('dotenv').config();
-const connection = require('~/config/database');
+require("dotenv").config();
+const connection = require("~/config/database");
 
-const apiRoutes = require('~/routes/');
-const configViewEngine = require('~/config/viewEngine');
+const apiRoutes = require("~/routes/");
+const configViewEngine = require("~/config/viewEngine");
 const port = process.env.PORT || 8888;
 
 const START_SERVER = () => {
-  const app = express()
+  const app = express();
 
   // Xử lý CORS
-  app.use(cors())
+  app.use(cors());
 
- 
-//config req.body
-app.use(express.json()) // for json
-app.use(express.urlencoded({ extended: true })) // for form data
+  //config req.body
+  app.use(express.json()); // for json
+  app.use(express.urlencoded({ extended: true })); // for form data
 
-//config template engine
-configViewEngine(app);
+  //config template engine
+  configViewEngine(app);
 
-//khai báo route
-app.use('/v1/api/', apiRoutes);
-// app.use('/', getHomepage);
+  //khai báo route
+  app.use("/v1/api/", apiRoutes);
+  // app.use('/', getHomepage);
 
-
-(async () => {
+  app.get("/", (req, res) => {
+    res.json("hello");
+  })(async () => {
     try {
-        //using mongoose
-        await connection();
+      //using mongoose
+      await connection();
 
-        app.listen(port, () => {
-            console.log(`Backend Nodejs App listening on port ${port}`)
-        })
+      app.listen(port, () => {
+        console.log(`Backend Nodejs App listening on port ${port}`);
+      });
     } catch (error) {
-        console.log(">>> Error connect to DB: ", error)
+      console.log(">>> Error connect to DB: ", error);
     }
-})()
+  })();
+};
 
-  
-}
-
-
-START_SERVER()
+START_SERVER();
